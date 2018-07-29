b96097898749957add75fb34af463321cc30862b
Eric Fischer <enf@pobox.com>
2018-07-28 23:41:21 -0700

    Copy setup boilerplate

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 dlib-face
 =========
+
+Dlib installation: https://gist.github.com/ageitgey/629d75c1baac34dfa5ca2a1928a7aeaf
---
+++ encode.cpp
@@ -0,0 +1,91 @@
+// Following:
+//
+// http://dlib.net/dnn_face_recognition_ex.cpp.html
+// https://github.com/ageitgey/face_recognition/blob/master/face_recognition/api.py
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <string>
+#include <dlib/dnn.h>
+#include <dlib/gui_widgets.h>
+#include <dlib/clustering.h>
+#include <dlib/string.h>
+#include <dlib/image_io.h>
+#include <dlib/image_processing/frontal_face_detector.h>
+
+using namespace dlib;
+
+std::string nextline() {
+	std::string out;
+
+	int c;
+	while ((c = getchar()) != EOF) {
+		out.push_back(c);
+
+		if (c == '\n') {
+			break;
+		}
+	}
+
+	return out;
+}
+
+// The next bit of code defines a ResNet network.  It's basically copied
+// and pasted from the dnn_imagenet_ex.cpp example, except we replaced the loss
+// layer with loss_metric and made the network somewhat smaller.  Go read the introductory
+// dlib DNN examples to learn what all this stuff means.
+//
+// Also, the dnn_metric_learning_on_images_ex.cpp example shows how to train this network.
+// The dlib_face_recognition_resnet_model_v1 model used by this example was trained using
+// essentially the code shown in dnn_metric_learning_on_images_ex.cpp except the
+// mini-batches were made larger (35x15 instead of 5x5), the iterations without progress
+// was set to 10000, and the training dataset consisted of about 3 million images instead of
+// 55.  Also, the input layer was locked to images of size 150.
+template <template <int,template<typename>class,int,typename> class block, int N, template<typename>class BN, typename SUBNET>
+using residual = add_prev1<block<N,BN,1,tag1<SUBNET>>>;
+
+template <template <int,template<typename>class,int,typename> class block, int N, template<typename>class BN, typename SUBNET>
+using residual_down = add_prev2<avg_pool<2,2,2,2,skip1<tag2<block<N,BN,2,tag1<SUBNET>>>>>>;
+
+template <int N, template <typename> class BN, int stride, typename SUBNET> 
+using block  = BN<con<N,3,3,1,1,relu<BN<con<N,3,3,stride,stride,SUBNET>>>>>;
+
+template <int N, typename SUBNET> using ares      = relu<residual<block,N,affine,SUBNET>>;
+template <int N, typename SUBNET> using ares_down = relu<residual_down<block,N,affine,SUBNET>>;
+
+template <typename SUBNET> using alevel0 = ares_down<256,SUBNET>;
+template <typename SUBNET> using alevel1 = ares<256,ares<256,ares_down<256,SUBNET>>>;
+template <typename SUBNET> using alevel2 = ares<128,ares<128,ares_down<128,SUBNET>>>;
+template <typename SUBNET> using alevel3 = ares<64,ares<64,ares<64,ares_down<64,SUBNET>>>>;
+template <typename SUBNET> using alevel4 = ares<32,ares<32,ares<32,SUBNET>>>;
+
+using anet_type = loss_metric<fc_no_bias<128,avg_pool_everything<
+                            alevel0<
+                            alevel1<
+                            alevel2<
+                            alevel3<
+                            alevel4<
+                            max_pool<3,3,2,2,relu<affine<con<32,7,7,2,2,
+                            input_rgb_image_sized<150>
+                            >>>>>>>>>>>>;
+
+int main(int argc, char **argv) {
+	dlib::frontal_face_detector detector = dlib::get_frontal_face_detector();
+
+	dlib::shape_predictor sp;
+	dlib::deserialize("shape_predictor_5_face_landmarks.dat") >> sp;
+
+	anet_type net;
+	dlib::deserialize("dlib_face_recognition_resnet_model_v1.dat") >> net;
+
+	while (true) {
+		std::string fname = nextline();
+
+		if (fname.size() == 0) {
+			break;
+		}
+
+		fname.resize(fname.size() - 1);
+	}
+}
